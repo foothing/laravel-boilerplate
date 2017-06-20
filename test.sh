7ff2fcc7663419e7b56ab3510d794e5ecb0050ef
brazorf <brazorf@users.noreply.github.com>
2017-06-20 17:11:25 +0200

    chore: test git hook.

--- test.sh
+++ test.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 revisioncount=`git log --oneline -n 1 | cut -d ' ' -f 1`
-#projectversion=`git describe --tags --long`
-cleanversion=${projectversion%%-*}
-
-echo "<?php \$_v = md5('$revisioncount');" > "./revision"
+echo "<?php \$_v = md5('$revisioncount');" > "../../revision"
