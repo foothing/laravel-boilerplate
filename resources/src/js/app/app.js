51c8c9714dbdb1f5325766d3f8f3fe5997750689
brazorf <fabrizio.ranieri@gmail.com>
2018-11-13 19:52:37 +0100

    feature: views in main app state.

--- resources/src/js/app/app.js
+++ resources/src/js/app/app.js
@@ -5,8 +5,84 @@
 		$urlRouterProvider.otherwise('/');
 
 		$stateProvider.state('root', {
-			url: '',
-			template: 'root state'
+            url: '',
+            views: {
+                "navbar": {
+                    templateUrl: 'partials/navbar.html',
+                    controller: function($scope, $state) {
+                        $scope.$state = $state;
+                    }
+                },
+
+                "main": {
+                    template: 'App main.'
+                },
+
+                "title": {
+                    template: 'App title.'
+                },
+
+                "user": {
+                    templateUrl: 'partials/navbar-user.html',
+                    controller: function($scope, $user) {
+                        $scope.user = $user.get();
+                    }
+                },
+
+                "notificationIcon": {
+                    templateUrl: 'partials/notifications-icon.html',
+                    controller: function($scope, $rootScope) {
+                        $rootScope.$on("notifications.updated", function(event, data){
+                            $scope.icon = false;
+                            check(data);
+                        });
+
+                        function check(notifications) {
+                            angular.forEach(notifications, function(item){
+                                if (! item.read_at) {
+                                    $scope.icon = true;
+                                }
+                            });
+                        }
+                    }
+                },
+
+                "notifications": {
+                    templateUrl: 'partials/notifications.html',
+                    controller: function($scope, $rootScope, notifications, api, $user, $state) {
+                        $scope.notifications = notifications;
+
+                        $rootScope.$emit("notifications.updated", notifications);
+
+                        // This because notifications html sucks
+                        // and doesn't allows for <a> inside the items.
+                        $scope.go = function() {
+                            $state.go('app.brand');
+                        }
+
+                        $scope.markAsRead = function(notification) {
+                            api.call('user.readNotifications', $user.get().id, {id: notification.id}).then(function(response){
+                                $scope.notifications = response;
+                                $rootScope.$emit("notifications.updated", response);
+                            });
+                        }
+
+                        $scope.markAllAsRead = function() {
+                            api.call('user.readAllNotifications', $user.get().id).then(function(response){
+                                $scope.notifications = response;
+                                $rootScope.$emit("notifications.updated", response);
+                            });
+                        }
+                    }
+                }
+            },
+
+            resolve: {
+                notifications: function(api, $user) {
+                    var user = $user.get();
+                    return user ? api.call('user.notifications', $user.get().id) : null;
+                }
+            }
 		});
 	});
 
